09e1e25420c57ddd9d73fc8d0057bcf1c1e64ba0
Williams <wilfre_padilla@hotmail.com>
2021-12-04 15:11:06 -0400

    changes

--- dist/actions/permissions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.overridePermissions = void 0;
-const overridePermissions = (browser, url, permissions = ['geolocation', 'notifications']) => {
-    const context = browser.defaultBrowserContext();
-    context.overridePermissions(url, permissions);
-};
-exports.overridePermissions = overridePermissions;
-//# sourceMappingURL=permissions.js.map
