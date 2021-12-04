3036390538eb8a916172ab4d9cbc0f07fdc83ea8
Williams <wilfre_padilla@hotmail.com>
2021-12-04 13:03:36 -0400

    port 80

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,5 @@ app.use('*', (req, res) => {
   res.send('<h1>Welcome to your simple server! Awesome right</h1>');
 });
 
-app.listen(process.env.API_PORT || 8080,
+app.listen(80,
   () => console.log('Server is running...'));
