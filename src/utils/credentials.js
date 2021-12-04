ba4c08459c75fdfe8c061ceae7028f773f789abe
Williams <wilfre_padilla@hotmail.com>
2021-12-04 15:15:00 -0400

    changes

--- src/utils/credentials.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.credentials = void 0;
-exports.credentials = {
-    fbCredentials: {
-        email: 'audsongs@hotmail.com',
-        pass: 'washicolesten', // fb password
-    },
-};
--- src/utils/urls.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const urls = {
-    facebookURL: 'https://www.facebook.com/',
-    facebookMarketPlace: 'https://www.facebook.com/marketplace/create/item',
-};
-exports.default = urls;
